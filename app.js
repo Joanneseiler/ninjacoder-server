2932212b756e1c43c5d8dd505958b106e7f6c27a
Anja Ramihone <AnjaRL@users.noreply.github.com>
2021-07-28 13:35:00 +0400

    build folder

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require("./db");
 const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
@@ -33,6 +33,9 @@ app.use(
   })
 );
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
@@ -52,6 +55,12 @@ app.use("/api", tutorRoutes);
 
 const cloudinaryRoutes = require("./routes/file-upload.routes");
 app.use("/api", cloudinaryRoutes);
+
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
